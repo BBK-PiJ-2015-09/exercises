19126c84e6991597fe2ecf4eaf6d5731e05b38b5
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2015-11-16 19:41:26 +0000

    Add 5.1_hash_function

---
+++ day8/5_maps/5.1_hash_function/HashUtilitiesSpec.java
@@ -0,0 +1,11 @@
+public class HashUtilitiesSpec {
+
+	public static void main(String[] args) {
+		System.out.println("Give me a string and I will calculate its hash code");
+		String str = System.console().readLine();
+		int hash = str.hashCode();
+		int smallHash = HashUtilities.shortHash(hash);
+		System.out.println("0 < " + smallHash + " < 1000");
+	}
+
+}
