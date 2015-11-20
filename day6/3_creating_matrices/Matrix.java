c9f01a2e1b81e9c314a8df4697d01b85c4c65838
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2015-11-20 20:07:14 +0000

    Add 3_creating_matrices

--- day6/3_creating_matrices/Matrix.java
+++ day6/3_creating_matrices/Matrix.java
@@ -1,22 +1,145 @@
+// A 2D Matrix //////
 public class Matrix {
-
-	private int[][] matrix;
 	
-	public Matrix(int width, int height) {
-		matrix = new int[width][height];
-		for(int i = 0; i < width; i++) {
-			for(int j = 0; j < height; j++) {
+	// Fields ///////////
+	private int[][] matrix;
+	private int rows;
+	private int columns;
+	
+	// Constructor //////
+	public Matrix(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		matrix = new int[rows][columns];
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < columns; j++) {
 				matrix[i][j] = 1;
 			}
 		}
 	}
 
-	public void setElement(int width, int height, int value) {
-		if (width > 0 && height > 0 && width <= matrix.length && height <= matrix[0].length) {
-			matrix[width][height] = value;
-		} else {
-			System.out.println("The coordinates must be within the matrix!");
+	// Setters //////////
+	public void setElement(int row, int column, int value) {
+		if (validReference(row, column)) {
+			matrix[row][column] = value;
+			prettyPrintSleepFlush();
 		}
+	}
+
+	public void setRow(int row, String values) {
+		String[] strArray = splitString(values);
+		int[] intArray = toInt(strArray);
+		if (valid(row, rows) && intArray.length == columns) {
+			for(int i = 0; i < intArray.length; i++) {
+				setElement(row, i, intArray[i]);
+			}
+		} else {
+			printOutOfBounds();
+		}
+	}
+	
+	public void setColumn(int column, String values) {
+		String[] strArray = splitString(values);
+		int[] intArray = toInt(strArray);
+		if (valid(column, columns) && intArray.length == columns) {
+			for(int i = 0; i < intArray.length; i++) {
+				setElement(i, column, intArray[i]);
+			}
+		} else {
+			printOutOfBounds();
+		}
+	}
+
+	public void fillMatrix(int value) {
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < columns; j++) {
+				setElement(i, j, value);
+			}
+		}
+	}
+
+	// Validators ///////
+	private boolean validReference(int row, int column) {
+		if (valid(row, rows) && valid(column, columns)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private boolean valid(int item, int set) {
+		if (item < set && item >= 0) {
+			return true;
+		} else {
+			printOutOfBounds();
+			return false;
+		}
+	}
+
+	// Type conversion //
+	private String[] splitString(String values) {
+		return values.split(",");
+	}
+
+	private int[] toInt(String[] strArray) {
+		int[] intArray = new int[strArray.length];
+		for(int i = 0; i < strArray.length; i++) {
+			intArray[i] = Integer.parseInt(strArray[i]);
+		}
+		return intArray;
+	}
+
+	public String toString() {
+		String str = "[";
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < columns; j++) {
+				str += matrix[i][j];
+			}
+			str += ";";
+		}
+		return str + "]";
+	}
+
+	// Alerts ///////////
+	private void printOutOfBounds() {
+		System.out.println("The coordinates must be within the matrix!");
+	}
+
+	// Printing /////////
+	private void prettyPrintSleepFlush() {
+		prettyPrint();
+		sleep();
+		flush();
+	}
+
+	private void prettyPrint() {
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < columns; j++) {
+				System.out.print(matrix[i][j] + "\t");
+			}
+			System.out.println("\n");
+		}
+	}
+
+	private void sleep() {
+		try {
+		  Thread.sleep(200);
+		} catch (InterruptedException ie) {
+		    // Handle any exceptions
+		}
+	}
+	
+	private void flush() {
+		try {
+			Runtime.getRuntime().exec("clear");
+		} catch (final Exception e) {
+			// Handle any exceptions
+		}
+		
+		final String ANSI_CLS = "\u001b[2J";
+        final String ANSI_HOME = "\u001b[H";
+        System.out.print(ANSI_CLS + ANSI_HOME);
+        System.out.flush();
 	}
 
 }
--- day6/3_creating_matrices/MatrixScript.java
+++ day6/3_creating_matrices/MatrixScript.java
@@ -1,9 +1,12 @@
 public class MatrixScript {
 
 	public static void main(String[] args) {
-		Matrix myMatrix = new Matrix(4,4);
-		myMatrix.setElement(1,2,3);
-		System.out.println(myMatrix);
+		Matrix myMatrix = new Matrix(10,10);
+		myMatrix.fillMatrix(3);
+		myMatrix.setRow(7, "95,96,97,8,8,8,8,8,8,8");
+		myMatrix.setColumn(2, "5,6,7,8,8,8,8,8,8,8");
+		System.out.println(myMatrix.toString());
+		
 	}
 
 }
