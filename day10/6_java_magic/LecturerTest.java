18f008bb88cc37f0d1c894e0c94280ecfc33600a
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2015-11-30 21:14:48 +0000

    Add 6_java_magic

---
+++ day10/6_java_magic/LecturerTest.java
@@ -0,0 +1,9 @@
+public class LecturerTest {
+	
+	public static void main(String[] args) {
+		Lecturer lecturer = new Lecturer("Roger");
+		lecturer.doResearch("Java");
+		lecturer.teach("Ruby");
+	}
+	
+}
