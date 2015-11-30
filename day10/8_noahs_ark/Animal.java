fdc98d302d735d4a65d4b203028b0d2596a8406d
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2015-11-30 22:55:05 +0000

    day10/8_noahs_ark - Add rudimentary Noah's Ark with basic bear

---
+++ day10/8_noahs_ark/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+	String name;
+	
+	public Animal(String name) {
+		this.name = name;
+	}
+	
+	public void call() {
+		System.out.println(name + " coming...");
+	}
+	
+}
---
+++ day10/8_noahs_ark/TwoByTwo.java
@@ -0,0 +1,8 @@
+public class TwoByTwo {
+	
+	public static void main(String[] args) {
+		Bear bear = new Bear();
+		bear.call();
+	}
+	
+}
