757a43921dac6932b73983ed20272559b57a9afc
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-01-04 19:39:13 +0000

    Add less verbose day11/1/ComparatorTest

--- day11/1_dont_repeat_yourself/ComparatorTest.java
+++ day11/1_dont_repeat_yourself/ComparatorTest.java
@@ -1,12 +1,19 @@
 public class ComparatorTest {
 
 	public static void main(String[] args) {
-		Comparator<Integer> intComparator = new Comparator<>();
-		System.out.println(intComparator.getMax(5,6));
-		Comparator<Double> doubleComparator = new Comparator<>();
-		System.out.println(doubleComparator.getMax(5.1,6.2));
-		Comparator<String> stringComparator = new Comparator<>();
-		System.out.println(stringComparator.getMax("5","6"));
+		// safe
+		// Comparator<Integer> intComparator = new Comparator<>();
+		// System.out.println(intComparator.getMax(5,6));
+		// Comparator<Double> doubleComparator = new Comparator<>();
+		// System.out.println(doubleComparator.getMax(5.1,6.2));
+		// Comparator<String> stringComparator = new Comparator<>();
+		// System.out.println(stringComparator.getMax("5","6"));
+		
+		// or even unsafe... (throws error but compiles & runs)
+		Comparator comparator = new Comparator();
+		System.out.println(comparator.getMax(5,6));
+		System.out.println(comparator.getMax(5.1,6.2));
+		System.out.println(comparator.getMax("5","6"));
 		
 	}
 
