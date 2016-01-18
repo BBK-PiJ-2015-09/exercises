bab7be050fe61b759d5f3a182c5c2c56b6f89082
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-01-18 18:26:23 +0000

    Add BookImpl with tests failing

---
+++ day13/1_practing_tdd/BookImpl.java
@@ -0,0 +1,17 @@
+public class BookImpl implements Book {
+	
+	public BookImpl(String author, String title) {
+		
+	}
+	
+	@Override
+	public String getAuthor() {
+		return "";
+	}
+	
+	@Override
+	public String getTitle() {
+		return "";
+	}
+
+}
--- day13/1_practing_tdd/BookTest.java
+++ day13/1_practing_tdd/BookTest.java
@@ -2,24 +2,28 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class BookTest {
+	Book book;
 	
 	@Before
 	public void buildUp() {
-		
+		book = new BookImpl("Jonathan Swift", "Gulliver's Travels");
 	}
 	@After
 	public void cleanUp() {
-		
 	}	
 		
 	@Test
 	public void testsGetAuthor() {
-		
+		String output = book.getAuthor();
+		String expected = "Jonathan Swift";
+		assertEquals(output, expected);
 	}
 	
 	@Test
 	public void testsGetTitle() {
-		
+		String output = book.getTitle();
+		String expected = "Gulliver's Travels";
+		assertEquals(output, expected);
 	}
 	
 }
