5c8fab1d6a6a9f8c2f8092376434fef85d3c1f6a
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-02-22 19:35:00 +0000

    Add Sergio's naive threading

--- day17/responsive_ui/Sergio's solution/ResponsiveUI.java
+++ day17/responsive_ui/Sergio's solution/ResponsiveUI.java
@@ -7,13 +7,15 @@ public class ResponsiveUI {
 
 	public void launch() {
 		boolean running = true;
+		// CompletedTaskCollector collector = new CompletedTaskCollector();
 		int taskId = 0;
 		for (int i = 0; i < 10; i++) {
 			System.out.print("Delay for next task in seconds: ");
 			int delay = Integer.parseInt(System.console().readLine());
 			taskId++;
 			Runnable task = new WaitingTask(taskId, 1000 * delay);
-			task.run();
+			Thread t = new Thread(task);
+			t.start();
 		}
 	}
 }
