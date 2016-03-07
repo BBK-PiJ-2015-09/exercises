5a28ace7db627343c0db638f0fc875b98a2c9c7e
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-07 19:44:05 +0000

    Add day20 questions 4 & 5

--- day20/StreamDemo.java
+++ day20/StreamDemo.java
@@ -4,11 +4,14 @@ import java.util.stream.*;
 public class StreamDemo {
 
 	public static void main(String[] args) {
-		List<String> words = Arrays.asList("hi", "hello", "dog", "cat", "shoe", "house", "tree");
+		List<String> words = Arrays.asList("hi", "hello", "dog", "cat", "shoe", "house", "tree", "briar", "eve", "quo");
+
 		System.out.println("\nQuestion 1:");
 		words.stream().forEach(e -> System.out.println("  " + e));
+
 		System.out.println("\nQuestion 2:");
 		words.stream().forEach(System.out::println);
+
 		System.out.println("\nQuestion 3:");
 		// StringUtils.transformedList(words, s -> s + "!");
 		words.stream().map(e -> e + "!").forEach(System.out::println);
@@ -16,6 +19,26 @@ public class StreamDemo {
 		words.stream().map(e -> e.replace("i", "eye")).forEach(System.out::println);
 		// StringUtils.transformedList(words, String::toUpperCase);
 		words.stream().map(String::toUpperCase).forEach(System.out::println);
+
+		System.out.println("\nQuestion 4:");
+		// StringUtils.allMatches(words, s -> s.length() < 4);
+		words.stream().filter(e -> e.length() < 4).forEach(System.out::println);
+		// StringUtils.allMatches(words, s -> s.contains("b"));
+		words.stream().filter(e -> e.contains("b")).forEach(System.out::println);
+		// StringUtils.allMatches(words, s -> (s.length() % 2) == 0);
+		words.stream().filter(e -> (e.length() % 2) == 0).forEach(System.out::println);
+
+		System.out.println("\nQuestion 5:");
+		words.stream()
+			.map(String::toUpperCase)
+			.filter(e -> e.length() < 4)
+			.filter(e -> e.contains("E"))
+			.forEach(System.out::println);
+		words.stream()
+			.map(String::toUpperCase)
+			.filter(e -> e.length() < 4)
+			.filter(e -> e.contains("Q"))
+			.forEach(System.out::println);
 	}
 
 }
