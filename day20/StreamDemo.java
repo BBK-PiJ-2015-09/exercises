2aa5b76d5fb51a303d8ca81a9c45876398c169dd
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-07 19:04:57 +0000

    Add day20/question 2

--- day20/StreamDemo.java
+++ day20/StreamDemo.java
@@ -5,8 +5,10 @@ public class StreamDemo {
 
 	public static void main(String[] args) {
 		List<String> words = Arrays.asList("hi", "hello", "dog", "cat", "shoe", "house", "tree");
-		Stream<String> wordsStream = words.stream();
-		wordsStream.forEach((n) -> System.out.println("  " + n));
+		System.out.println("Question 1:");
+		words.stream().forEach((n) -> System.out.println("  " + n));
+		System.out.println("Question 2:");
+		words.stream().forEach(System.out::println);
 	}
 
 }
