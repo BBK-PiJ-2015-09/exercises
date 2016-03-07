c50c0e0b1d5d100722400bce7ca0848c5daefc57
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2016-03-07 19:36:57 +0000

    Add day20/question 3

--- day20/StreamDemo.java
+++ day20/StreamDemo.java
@@ -5,10 +5,17 @@ public class StreamDemo {
 
 	public static void main(String[] args) {
 		List<String> words = Arrays.asList("hi", "hello", "dog", "cat", "shoe", "house", "tree");
-		System.out.println("Question 1:");
-		words.stream().forEach((n) -> System.out.println("  " + n));
-		System.out.println("Question 2:");
+		System.out.println("\nQuestion 1:");
+		words.stream().forEach(e -> System.out.println("  " + e));
+		System.out.println("\nQuestion 2:");
 		words.stream().forEach(System.out::println);
+		System.out.println("\nQuestion 3:");
+		// StringUtils.transformedList(words, s -> s + "!");
+		words.stream().map(e -> e + "!").forEach(System.out::println);
+		// StringUtils.transformedList(words, s -> s.replace("i", "eye"));
+		words.stream().map(e -> e.replace("i", "eye")).forEach(System.out::println);
+		// StringUtils.transformedList(words, String::toUpperCase);
+		words.stream().map(String::toUpperCase).forEach(System.out::println);
 	}
 
 }
